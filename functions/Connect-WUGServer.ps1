1ae83904c1a214a1887d1ff64666b25daa89ad25
Jason Alberino <jason@wug.ninja>
2023-02-17 13:25:19 -0500

    Fixed token expiration errors

--- functions/Connect-WUGServer.ps1
+++ functions/Connect-WUGServer.ps1
@@ -1,3 +1,40 @@
+<#
+.SYNOPSIS
+    Connect to a WhatsUp Gold server to obtain an authorization token to
+     use for REST API requests
+
+.DESCRIPTION
+    Obtains a WhatsUp Gold REST API authorization token
+
+.PARAMETER serverUri
+    The IP address or fully-qualified domain name of the WhatsUp Gold server.
+
+.PARAMETER Protocol
+    Specify whether to use HTTP or HTTPS. HTTP is default.
+
+.PARAMETER Username
+    Plaintext username used to connect to the WhatsUp Gold Server
+    INSECURE, USE AT YOUR OWN RISK! Use -Credential instead!
+
+.PARAMETER Password
+    Plaintext password used to connect to the WhatsUp Gold Server
+    INSECURE, USE AT YOUR OWN RISK! Use -Credential instead!
+
+.PARAMETER Credential
+    Accepts a PowerShell credential object. Set your credential first.
+    For example: $Credential = Get-Credential
+
+.NOTES
+    WhatsUp Gold REST API Handling Session Tokens
+    https://docs.ipswitch.com/NM/WhatsUpGold2022_1/02_Guides/rest_api/#section/Handling-Session-Tokens
+
+.EXAMPLE
+    Connect-WUGServer
+    Connect-WUGServer -serverUri 192.168.1.212 -Credential $Credential -Protocol https
+    Connect-WUGServer -serverUri 192.168.1.212 -Username "admin"
+    Connect-WUGServer -serverUri 192.168.1.212 -Username "admin" -Password "Password"
+    Connect-WUGServer -server 192.168.1.212 -Username user -Password pass -Protocol https
+#>
 function Connect-WUGServer {
     param (
         [Parameter(Mandatory)] [string] $serverUri,
@@ -33,9 +70,9 @@ function Connect-WUGServer {
     $tokenUri = "${protocol}://${serverUri}:9644/api/v1/token"
     $tokenHeaders = @{"Content-Type" = "application/json"}
     $tokenBody = "grant_type=password&username=${Username}&password=${Password}"
+
     try {
         $token = Invoke-RestMethod -Uri $tokenUri -Method Post -Headers $tokenHeaders -Body $tokenBody -SkipCertificateCheck
-        $token
     } catch {
         $message = "Error: $($_.Exception.Response.StatusDescription) `n URI: $tokenUri"
         Write-Error -message $message
@@ -44,9 +81,9 @@ function Connect-WUGServer {
 
     $global:WUGBearerHeaders = @{
         "Content-Type" = "application/json"
-        "Authorization" = "Bearer $($token.access_token)"
+        "Authorization" = "$($token.token_type) $($token.access_token)"
     }
 
-    $global:expiry = (Get-Date).AddSeconds($token.expires_in).ToUniversalTime().ToString("s")
+    $global:expiry = (Get-Date).AddSeconds($token.expires_in)
     return "Connected to ${serverUri} to obtain authorization token for user `"${Username}`" which expires at $global:expiry UTC."
 }
--- functions/Get-WUGAPIResponse.ps1
+++ functions/Get-WUGAPIResponse.ps1
@@ -1,16 +1,41 @@
+<#
+.SYNOPSIS
+    Connect to a REST API endpoint by specifying a full URI and a  HTTP Verb
+
+.DESCRIPTION
+    Connect to a REST API endpoint by specifying a full URI and a  HTTP Verb
+
+.PARAMETER Uri
+    The entire API endpoint, for example, http://192.168.1.212:9644/api/v1/product/api
+
+.PARAMETER Method
+    The HTTP verb to use when connecting to the REST API endpoint.
+    Valid options are:
+    GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH
+
+.EXAMPLE
+    Get-WUGAPIResponse -uri "http://192.168.1.212:9644/api/v1/product/api" -Method GET
+    $dataObject = (Get-WUGAPIResponse -uri "http://192.168.1.212:9644/api/v1/product/api" -Method GET).data
+
+#>
 function Get-WUGAPIResponse {
     param(
         [Parameter()] [string] $Uri,
         [Parameter()] [ValidateSet('GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH')] [string] $Method
     )
-    if (-not $global:WUGBearerHeaders -or $Global:expiry -le (Get-Date)) {
+    $global:expiry
+    if (-not $global:WUGBearerHeaders){
+        Connect-WUGServer
+    }
+    if ((Get-Date) -ge $global:expiry){
+        Write-Error -Message "Token expired, running Connect-WUGServer"
         Connect-WUGServer
     }
     if (-not $Uri){
         $Uri = Read-Host "Enter the fully qualified REST API endpoint."
     }
     If (-not $Method){
-        $Method = Read-Host "Enter the HTTP verb to use (GET, POST, PUT, PATCH)."
+        $Method = Read-Host "Enter the HTTP verb to use (GET, POST, PUT, DELETE, PATCH, CONNECT, OPTIONS, TRACE, HEAD)."
     }
     try {
         $response = Invoke-RestMethod -Uri $Uri -Method $Method -Headers $global:WUGBearerHeaders

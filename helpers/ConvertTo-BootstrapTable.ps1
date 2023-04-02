4ef0660b109ad46b031e81a84143887a770814bc
Jason Alberino <jason@wug.ninja>
2023-04-02 00:06:29 -0400

    Changes

--- helpers/ConvertTo-BootstrapTable.ps1
+++ helpers/ConvertTo-BootstrapTable.ps1
@@ -13,16 +13,17 @@ function ConvertTo-BootstrapTable {
             field = $_.Name
             title = $_.Name
         }
-
         if ($_.Name -eq 'downActiveMonitors') {
             $column.formatter = 'formatDownActiveMonitors'
         }
-
+        if ($_.Name -eq 'id') {
+            $column.formatter = 'formatId'
+        }
         $columns += $column
     }
 
-    $columnsJson = $columns | ConvertTo-Json -Compress
-    $dataJson = $jsonData | ConvertTo-Json -Compress
+    $columnsJson = $columns | ConvertTo-Json -Depth 5 -Compress
+    $dataJson = $jsonData | ConvertTo-Json -Depth 5 -Compress
 
     return @"
         columns: $columnsJson,

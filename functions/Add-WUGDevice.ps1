d4f30f8d5ff55e5cefc8eddb26843bee3de50e44
YouHaveToBeKiddingMe <149519302+YouHaveToBeKiddingMe@users.noreply.github.com>
2024-02-09 12:44:49 -0600

    Update Add-WUGDevice.ps1
    Fixed/added code to handle SubRoles for empty or an array of strings.

--- functions/Add-WUGDevice.ps1
+++ functions/Add-WUGDevice.ps1
@@ -120,7 +120,7 @@ function Add-WUGDevice {
         [Parameter()] <#[ValidateSet("Workstation", "Server")]#> [string] $deviceType,
         [Parameter()] [ValidateRange(10, 3600)] [int] $PollInterval = 60,
         [Parameter()] <#[ValidateSet("Device", "Router", "Switch", "Firewall")]#> [string] $PrimaryRole = "Device",
-        [Parameter()] [ValidateNotNullOrEmpty()] [array] $SubRoles,
+        [Parameter()] <#[ValidateNotNullOrEmpty()]#> [array] $SubRoles,
         [Parameter()] [ValidateNotNullOrEmpty()] [string] $snmpOid,
         [Parameter()] [ValidateRange(1, 65535)] [int] $SNMPPort,
         [Parameter()] [ValidateNotNullOrEmpty()] <#[ValidateSet("Windows", "Linux", "Unix")]#> [string] $OS,
@@ -175,10 +175,12 @@ function Add-WUGDevice {
     #End global variables error checking
 
     #Begin Input validation
-    if ($SubRoles) { if ($SubRoles -isnot [string[]]) { throw "SubRoles parameter must be an array of strings." } }
+    if ($SubRoles) { if ($SubRoles -is [array]) { foreach ($item in $SubRoles) { if ($item -isnot [string]) { throw "SubRoles parameter must be a one-dimensional array of strings." } } } else { throw "ActiveMonitors parameter must be a one-dimensional array of strings." } }
+    #if ($SubRoles) { if ($SubRoles -isnot [string[]]) { throw "SubRoles parameter must be an array of strings." } }
     if ($ActiveMonitors) { if ($ActiveMonitors -is [array]) { foreach ($item in $ActiveMonitors) { if ($item -isnot [string]) { throw "ActiveMonitors parameter must be a one-dimensional array of strings." } } } else { throw "ActiveMonitors parameter must be a one-dimensional array of strings." } }
     if ($PerformanceMonitors) { if ($PerformanceMonitors -is [array]) { foreach ($item in $PerformanceMonitors) { if ($item -isnot [string]) { throw "PerformanceMonitors parameter must be a one-dimensional array of strings." } } } else { throw "PerformanceMonitors parameter must be a one-dimensional array of strings." } }
-    if ($PassiveMonitors) { if ($PassiveMonitors -is [array]) { foreach ($item in $PassiveMonitors) { if ($item -isnot [string]) { throw "PassiveMonitors parameter must be a one-dimensional array of strings." } } } else { throw "PassiveMonitors parameter must be a one-dimensional array of strings." } }   
+    if ($PassiveMonitors) { if ($PassiveMonitors -is [array]) { foreach ($item in $PassiveMonitors) { if ($item -isnot [string]) { throw "PassiveMonitors parameter must be a one-dimensional array of strings." } } } else { throw "PassiveMonitors parameter must be a one-dimensional array of strings." } }
+    if ($Credentials) { if ($Credentials -is [array]) { foreach ($item in $Credentials) { if ($item -isnot [string]) { throw "Credentials parameter must be a one-dimensional array of strings." } } } else { throw "Credentials parameter must be a one-dimensional array of strings." } }
     #End input validation
 
     #Begin data formatting
@@ -227,6 +229,17 @@ function Add-WUGDevice {
             $PassiveMonitorObjects += $PassiveMonitorObject
         }
     }
+    ### Credentials
+    $CredentialObjects = @()
+    if ($Credentials) {
+        foreach ($Credential in $Credentials) {
+            $CredentialObject = New-Object -TypeName PSObject -Property @{
+                credentialType = ''
+                credential     = $Credential
+            }
+            $CredentialObjects += $CredentialObject
+        }
+    }
     #End data formatting
 
     $options = @("all")
@@ -236,6 +249,9 @@ function Add-WUGDevice {
     if ($UpdateInterfaceNames) { $options += "update-interface-names" }
     if ($UpdateActiveMonitors) { $options += "update-active-monitors" }
     if (!$hostname) { $hostname = $DeviceAddress }
+    if (!$Brand) { $Brand = "Not Set" }
+    if (!$OS) { $OS = "Not Set" }
+    if (!$SNMPPort) { $SNMPPort = 161 }
 
     #Handle null objects
 
@@ -245,17 +261,17 @@ function Add-WUGDevice {
             templateId          = "WhatsUpGoldPS"
             displayName         = "${displayName}"
             deviceType          = "${deviceType}"
-            snmpOid             = ""
-            snmpPort            = ""
+            snmpOid             = "${snmpOid}"
+            snmpPort            = "${SNMPPort}"
             pollInterval        = "${PollInterval}"
             primaryRole         = "${PrimaryRole}"
-            subRoles            = @("Resource Attributes", "Resource Monitors")
-            os                  = ""
-            brand               = ""
-            actionPolicy        = ""
+            subRoles            = (&{if(-not $SubRoles){@()}else{@(,$SubRoles)}})
+            os                  = "${OS}"
+            brand               = "${Brand}"
+            actionPolicy        = "${ActionPolicy}"
             note                = "${note}"
             autoRefresh         = "$true"
-            credentials         = @()
+            credentials         = @(${CredentialObjects})
             interfaces          = @(
                 @{
                     defaultInterface     = "true"
